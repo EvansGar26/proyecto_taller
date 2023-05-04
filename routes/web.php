45b494b50c9b9cc965827857b8b08360a3534b6c
EvansGar26 <joshuaone26@outlook.com>
2023-05-04 09:57:13 -0600

    Rutas en middleware

--- routes/web.php
+++ routes/web.php
@@ -128,54 +128,54 @@ Route::middleware('auth')->group(function () {
 });
 
 
-//Ruta para pdfs
+// //Ruta para pdfs
 
-Route::get('/pdf', [PdfController::class, 'generarPdfUniCar'])->name('pdf.generarUniCar');
-Route::get('/pdf1', [PdfController::class, 'generarPdfCarFav'])->name('pdf.generarCarFav');
-Route::get('/pdf2', [PdfController::class, 'generarPdfAsiCon'])->name('pdf.generarAsiCon');
+// Route::get('/pdf', [PdfController::class, 'generarPdfUniCar'])->name('pdf.generarUniCar');
+// Route::get('/pdf1', [PdfController::class, 'generarPdfCarFav'])->name('pdf.generarCarFav');
+// Route::get('/pdf2', [PdfController::class, 'generarPdfAsiCon'])->name('pdf.generarAsiCon');
 
-//Ruta para graficas
+// //Ruta para graficas
 
-Route::get('/graficas/carreras2', [GraficasController::class, 'carreras'])->name('graficas-carreras');
-Route::get('/graficas/carreras', [GraficasController::class, 'carrerasVista'])->name('graf-carreras');
+// Route::get('/graficas/carreras2', [GraficasController::class, 'carreras'])->name('graficas-carreras');
+// Route::get('/graficas/carreras', [GraficasController::class, 'carrerasVista'])->name('graf-carreras');
 
-Route::get('/graficas/conferencias2', [GraficasController::class, 'conferencias'])->name('graficas-conferencias');
-Route::get('/graficas/conferencias', [GraficasController::class, 'conferenciasVista'])->name('graf-conferencias');
+// Route::get('/graficas/conferencias2', [GraficasController::class, 'conferencias'])->name('graficas-conferencias');
+// Route::get('/graficas/conferencias', [GraficasController::class, 'conferenciasVista'])->name('graf-conferencias');
 
-//Ruta para perfil
-//perro._.
+// //Ruta para perfil
+// //perro._.
 
-Route::get('/perfil/datos', [PerfilController::class, 'datos'])->name('datos-user');
-Route::patch('/perfil', [PerfilController::class, 'actualiza'])->name('actualiza-user');
-Route::get('/perfil/conferencias', [PerfilController::class, 'conferencias'])->name('conferencias-user');
-Route::get('/perfil/carreras-favoritas', [PerfilController::class, 'carreras'])->name('favoritas-user');
-Route::post('/perfil/carreras-favoritas', [PerfilController::class, 'destroy'])->name('elimina-carrera');
+// Route::get('/perfil/datos', [PerfilController::class, 'datos'])->name('datos-user');
+// Route::patch('/perfil', [PerfilController::class, 'actualiza'])->name('actualiza-user');
+// Route::get('/perfil/conferencias', [PerfilController::class, 'conferencias'])->name('conferencias-user');
+// Route::get('/perfil/carreras-favoritas', [PerfilController::class, 'carreras'])->name('favoritas-user');
+// Route::post('/perfil/carreras-favoritas', [PerfilController::class, 'destroy'])->name('elimina-carrera');
 
-Route::get('/perfil', function () {
-    return view('Perfil.perfil');
-})->name('perfil');
-
-
-//Ruta ajax para confirmar asistencia
-Route::post('/conferencias1/asistencia', [Asistencia_conferenciasController::class, 'agrega'])->name('confirma-asistencia');
+// Route::get('/perfil', function () {
+//     return view('Perfil.perfil');
+// })->name('perfil');
 
 
-//Rutas de tecnologias e ingenierias
-Route::get('/tecnologias', [TecnoController::class, 'index'])->middleware('auth')->name('despliega');
-Route::post('/tecnologias', [TecnoController::class, 'selecciona'])->name('selecciona');
-
-//Rutas de tecnologias e ingenierias
-Route::get('/ciencias', [CienciasController::class, 'index'])->middleware('auth')->name('despliega1');
-Route::post('/ciencias', [CienciasController::class, 'selecciona'])->name('selecciona1');
-
-Route::post('/algo', [Carreras_favoritasController::class, 'store'])->name('favorito');
-Route::post('/algo-delete', [Carreras_favoritasController::class, 'destroy'])->name('favorito-quitar');
+// //Ruta ajax para confirmar asistencia
+// Route::post('/conferencias1/asistencia', [Asistencia_conferenciasController::class, 'agrega'])->name('confirma-asistencia');
 
 
-// Rutas de login y registro
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'authenticate'])->name('inicio-sesion');
-Route::post('/', [AuthController::class, 'logout'])->name('cierra-sesion');
+// //Rutas de tecnologias e ingenierias
+// Route::get('/tecnologias', [TecnoController::class, 'index'])->middleware('auth')->name('despliega');
+// Route::post('/tecnologias', [TecnoController::class, 'selecciona'])->name('selecciona');
+
+// //Rutas de tecnologias e ingenierias
+// Route::get('/ciencias', [CienciasController::class, 'index'])->middleware('auth')->name('despliega1');
+// Route::post('/ciencias', [CienciasController::class, 'selecciona'])->name('selecciona1');
+
+// Route::post('/algo', [Carreras_favoritasController::class, 'store'])->name('favorito');
+// Route::post('/algo-delete', [Carreras_favoritasController::class, 'destroy'])->name('favorito-quitar');
+
+
+// // Rutas de login y registro
+// Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+// Route::post('/login', [AuthController::class, 'authenticate'])->name('inicio-sesion');
+// Route::post('/', [AuthController::class, 'logout'])->name('cierra-sesion');
 
 Route::get('/', function () {
     return view('template.index');
@@ -205,32 +205,32 @@ Route::get('login', function () {
 // //     return view('template.conferencias1');
 // // });
 
-Route::resource('registro', RegistroController::class);
+// Route::resource('registro', RegistroController::class);
 
-Route::resource('conferencias1', MuestraConferenciasController::class);
+// Route::resource('conferencias1', MuestraConferenciasController::class);
 
-Route::resource('paises', PaisesController::class);
+// Route::resource('paises', PaisesController::class);
 
-Route::resource('entidades', EntidadesController::class);
+// Route::resource('entidades', EntidadesController::class);
 
-Route::resource('municipios', MunicipiosController::class);
+// Route::resource('municipios', MunicipiosController::class);
 
-Route::resource('universidades', UniversidadesController::class);
+// Route::resource('universidades', UniversidadesController::class);
 
-Route::resource('universidades_imagenes', Universidades_imagenesController::class);
+// Route::resource('universidades_imagenes', Universidades_imagenesController::class);
 
-Route::resource('carreras', CarrerasController::class);
+// Route::resource('carreras', CarrerasController::class);
 
-Route::resource('carreras_imagenes', Carreras_imagenesController::class);
+// Route::resource('carreras_imagenes', Carreras_imagenesController::class);
 
-Route::resource('carreras_universidades', Carreras_universidadesController::class);
+// Route::resource('carreras_universidades', Carreras_universidadesController::class);
 
-Route::resource('roles', RolesController::class);
+// Route::resource('roles', RolesController::class);
 
-Route::resource('users', UsersController::class);
+// Route::resource('users', UsersController::class);
 
-Route::resource('grupos', GruposController::class);
+// Route::resource('grupos', GruposController::class);
 
-Route::resource('conferencias', ConferenciasController::class);
+// Route::resource('conferencias', ConferenciasController::class);
 
-Route::resource('conferencias_grupos', Conferencias_gruposController::class);
+// Route::resource('conferencias_grupos', Conferencias_gruposController::class);
